1c9cedd66b3e5db86890666e1b32c6130d8567e4
leoleung2000 <leoleung2000@yahoo.com.hk>
2020-02-24 22:12:20 +0800

    Update the rename code

--- file_renamer.py
+++ file_renamer.py
@@ -1,2 +1,12 @@
+import os
+import glob
 from hanziconv import HanziConv
 print(HanziConv.toTraditional('繁简转换器'))
+
+base_path = '/home/leoleung/Documents/FileRenamer/Target/'
+
+files = [f for f in glob.glob(base_path + "**/*.*", recursive=True)]
+
+for f in files:
+    print(f)
+    os.rename(f,HanziConv.toTraditional(f))
